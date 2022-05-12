29e2b6fa76fbfc57cae58fd9fca7aa8a90c83a71
Alex Grigorev <persian89g@gmail.com>
2022-05-12 19:29:11 +0300

    Initial commit

---
+++ src/main/java/ru/gb/java_core_3/l1_generics/GenericInterface.java
@@ -0,0 +1,5 @@
+package ru.gb.java_core_3.l1_generics;
+
+public interface GenericInterface<M> {
+    M doSomething(M request);
+}
---
+++ src/main/java/ru/gb/java_core_3/l1_generics/GenericsExample.java
@@ -0,0 +1,4 @@
+package ru.gb.java_core_3.l1_generics;
+
+public class GenericsExample {
+}
