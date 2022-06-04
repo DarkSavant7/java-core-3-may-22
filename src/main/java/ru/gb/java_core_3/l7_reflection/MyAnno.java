03194206dea884141384e404ca1761174be7eabc
Alex Grigorev <persian89g@gmail.com>
2022-06-04 12:54:55 +0300

    reflection

---
+++ src/main/java/ru/gb/java_core_3/l7_reflection/MyAnno.java
@@ -0,0 +1,12 @@
+package ru.gb.java_core_3.l7_reflection;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.TYPE, ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface MyAnno {
+    int someParam() default 14;
+}
